fbeb2453070dbd2ae2b3d78829390c6d76d33956
BorderExpander <ruax90@gmail.com>
2022-12-04 17:02:50 +0300

    Lesson 2

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String sex = "Male";
+    String name = "Mike";
+    int weight = 75;
+    int height = 190;
+    int age = 30;
+
+    public void walk() {
+        System.out.println("Идти");
+    }
+
+    public void sit() {
+        System.out.println("Сидеть");
+    }
+
+    public void run() {
+        System.out.println("Бежать");
+    }
+
+    public void talk() {
+        System.out.println("Говорить");
+    }
+
+    public void learJava() {
+        System.out.println("Учить JAVA");
+    }
+}
