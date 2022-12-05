42cbe0909a3b97852bacc3becd9ced59a8df4931
BorderExpander <ruax90@gmail.com>
2022-12-05 12:38:28 +0300

    корректировка +

--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -9,7 +9,7 @@ public class WolfTest {
         wolf.setColor("Черный");
 
         System.out.println("Мы создали волка по имени " + wolf.name + ", пол " + wolf.sex 
-                +" ,весом в " + wolf.weight + ", в возрасте 4" + ", окрас " + wolf.color);
+                + " ,весом в " + wolf.weight + ", в возрасте 4" + ", окрас " + wolf.color);
 
         wolf.walk();
         wolf.sit();
