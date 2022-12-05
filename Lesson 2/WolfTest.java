4fe9f2a62e0ac45c4fa285dc083e1cc9c0ca80a0
BorderExpander <ruax90@gmail.com>
2022-12-05 12:24:51 +0300

    геттеры/сеттеры

--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -2,11 +2,11 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.sex = "Мужской";
-        wolf.name = "Вульф";
-        wolf.weight = 50;
-        wolf.age = 4;
-        wolf.color = "Черный";
+        wolf.setSex("Мужской");
+        wolf.setName("Вульф");
+        wolf.setWeight(50);
+        wolf.setAge(4);
+        wolf.setColor("Черный");
 
         System.out.println("Мы создали волка по имени " + wolf.name + ", пол " + wolf.sex 
                 +" ,весом в " + wolf.weight + ", в возрасте 4" + ", окрас " + wolf.color);
