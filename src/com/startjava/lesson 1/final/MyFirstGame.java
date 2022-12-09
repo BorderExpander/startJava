de1863011f376ceaaca297e7fdf0905f086fa4df
BorderExpander <ruax90@gmail.com>
2022-12-09 14:46:26 +0300

    Итоговое ДЗ. Урок 3

---
+++ src/com/startjava/lesson 1/final/MyFirstGame.java
@@ -0,0 +1,27 @@
+package com.startjava.lesson_1.final
+
+public class MyFirstGame {
+
+    public static void main(String[] args) {
+        int guessNum = 49;
+        int suggestedNum = 0;
+        int counter = 0;
+
+        while (suggestedNum != guessNum) {
+            counter++;
+            suggestedNum = suggestedNum + (counter % 10);
+            if (suggestedNum < 1) {
+                suggestedNum = (counter % 10) + 1;
+            } else if (suggestedNum > 100) {
+                suggestedNum = 0;
+            }
+            if (suggestedNum > guessNum) {
+                System.out.println("Число " + suggestedNum + " больше того, что загадал компьютер");
+            } else if (suggestedNum < guessNum) {
+                System.out.println("Число " + suggestedNum + " меньше того, что загадал компьютер");
+            }
+        }
+
+        System.out.println("Вы победили! Загаданное число: " + guessNum);
+    }
+}
