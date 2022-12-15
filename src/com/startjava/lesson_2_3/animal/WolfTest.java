d8711ef15734102b380e858c17b746eabe13adaf
BorderExpander <ruax90@gmail.com>
2022-12-15 21:32:30 +0300

    Итоговое ДЗ. Урок 3. исправление замечаний

---
+++ src/com/startjava/lesson_2_3/animal/WolfTest.java
@@ -0,0 +1,22 @@
+package com.startjava.lesson_2_3.animal
+
+public class WolfTest {
+
+    public static void main(String[] args) {
+        Wolf wolf = new Wolf();
+        wolf.setSex("Мужской");
+        wolf.setName("Вульф");
+        wolf.setWeight(50);
+        wolf.setAge(4);
+        wolf.setColor("Черный");
+
+        System.out.println("Мы создали волка по имени " + wolf.name + ", пол " + wolf.sex 
+                + " ,весом в " + wolf.weight + ", в возрасте 4" + ", окрас " + wolf.color);
+
+        wolf.walk();
+        wolf.sit();
+        wolf.run();
+        wolf.hunt();
+        wolf.howl();
+    }
+}
---
+++ src/com/startjava/lesson_2_3/guess/GuessNumber.java
@@ -0,0 +1,43 @@
+package com.startjava.lesson_2_3.guess;
+import java.util.Scanner;
+
+public class GuessNumber {
+
+    private int guessNum;
+    private Player player1;
+    private Player player2;
+    private Scanner scan = new Scanner(System.in);
+
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void play() {
+        guessNum = (int) (1 + (Math.random() * 100));
+        while (true) {
+            if ( (isGuessed(player1)) || (isGuessed(player2)) ) {
+                break;
+            }
+        }
+    }
+
+    private boolean isGuessed(Player player) {
+        inputNum(player);
+        int num = player.getNum();
+        if (num == guessNum) {
+            System.out.println("Игрок " + player.getName() + " победил! Загаданное число: " + guessNum);
+            return true;
+        } if (num > guessNum) {
+            System.out.println("Число " + player.getNum() + " больше того, что загадал компьютер");
+        } else if (num < guessNum) {
+            System.out.println("Число " + player.getNum() + " меньше того, что загадал компьютер");
+        }
+        return false;
+    }
+
+    private void inputNum(Player player) {
+        System.out.println("Игрок " + player.getName() + " введите число:");
+        player.setNum(scan.nextInt());
+    }
+}
